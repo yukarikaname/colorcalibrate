a1d4b149788350b75e66cd772c7d7dd64607998f
Yukari Kaname <yukari@aliciaworks.com>
2026-03-19 11:30:16 +0800

    Initial Commit

---
+++ colorcalibrate/colorcalibrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorcalibrateApp.swift
+//  colorcalibrate
+//
+//  Created by Yukari Kaname on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct colorcalibrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
